ebf688cc2d1e2465a60dc6e046b3dc95adf4d3f6
Elizabeth Onyenekwe <lizzy@Elizabeths-MacBook-Air.local>
2022-09-23 23:08:54 +0100

    Did sign in with google

---
+++ components/upload.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
+
+const upload = () => {
+  return <div>upload</div>;
+};
+
+export default upload;
